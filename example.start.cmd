843fe258e91267c5559d55137491703e07e78625
pallid <1005220@mail.ru>
2018-08-24 21:40:52 +0300

    start

---
+++ example.start.cmd
@@ -0,0 +1,15 @@
+
+REM Для отладки
+REM SET TELEGRAM_ADDRESS=https://myproxyaddres
+SET TELEGRAM_BOT_TOKEN=my-telegram-token
+SET TELEGRAM_MESSAGE_FORMAT=Markdown
+
+REM SET BACKENDLESS_ADDRESS=https://myproxyaddres
+REM SET BACKENDLESS_APP_ID=my-backendeless-app-id
+REM SET BACKENDLESS_APP_TOKEN=my-backendeless-token
+
+SET LOGGING_LEVEL=DEBUG
+SET LOGGING_CONSOLE=true
+
+cd /d c:\projects\example_telegrambot\src
+C:\osweb-night\OneScript.WebHost.exe
